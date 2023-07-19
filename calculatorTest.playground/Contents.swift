184f82d0ea2b218c2bdcd38a2ffe4e71899f90a9
jooYeon <conn0204@naver.com>
2023-07-19 17:44:27 +0900

    AbstractOperation 생성 후, 연산 클래스들이 상속받도록 수정

--- calculatorTest.playground/Contents.swift
+++ calculatorTest.playground/Contents.swift
@@ -1,40 +1,48 @@
-class AddOperation {
+//연산 클래스에서 하는 일
+//각 숫자들을 연산하기
+
+class AbstractOperation {
     func result(firstNumber: Double, secondNumber: Double) -> Double {
+        return 0.0
+    }
+}
+
+class AddOperation: AbstractOperation {
+    override func result(firstNumber: Double, secondNumber: Double)  -> Double {
         return firstNumber + secondNumber
     }
 }
 
-class SubstractOperation {
-    func result(firstNumber: Double, secondNumber: Double) -> Double {
+class SubstractOperation: AbstractOperation {
+    override func result(firstNumber: Double, secondNumber: Double)  -> Double {
         return firstNumber - secondNumber
     }
 }
 
-class MultiplyOperation {
-    func result(firstNumber: Double, secondNumber: Double) -> Double {
-        return firstNumber * secondNumber
+class MultiplyOperation: AbstractOperation {
+    override func result(firstNumber: Double, secondNumber: Double)  -> Double {
+        return firstNumber - secondNumber
     }
 }
 
-class DivideOperation {
-    func result(firstNumber: Double, secondNumber: Double) -> Double {
-        return firstNumber / secondNumber
+class DivideOperation: AbstractOperation {
+    override func result(firstNumber: Double, secondNumber: Double)  -> Double {
+        return firstNumber - secondNumber
     }
 }
+
+//계산기가 하는 일
+//1. 숫자와 연산자 입력 받기
+//2. 연산한 결과 출력하기
 
 class Calculator {
-    let firstNumber: Double
-    let secondNumber: Double
+    let operation: AbstractOperation
     
-    init(firstNumber: Double, secondNumber: Double) {
-        self.firstNumber = firstNumber
-        self.secondNumber = secondNumber
+    init(operation: AbstractOperation) {
+        self.operation = operation
     }
     
-    func result() -> Double {
-        var result: Double = 0.0
-        
-        return result
+    func showResult(firstNumber: Double, secondNumber: Double) {
+        print(operation.result(firstNumber: firstNumber, secondNumber: secondNumber))
     }
  }
-
