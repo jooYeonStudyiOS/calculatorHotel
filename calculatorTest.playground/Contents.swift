f3e77682197836c2123c8fa8a3f7e3a1a3270903
jooYeon <conn0204@naver.com>
2023-07-19 17:21:05 +0900

    enum 삭제

--- calculatorTest.playground/Contents.swift
+++ calculatorTest.playground/Contents.swift
@@ -1,7 +1,3 @@
-enum Operation {
-    case add, substract, multiply, divide
-}
-
 class AddOperation {
     func result(firstNumber: Double, secondNumber: Double) -> Double {
         return firstNumber + secondNumber
@@ -29,10 +25,8 @@ class DivideOperation {
 class Calculator {
     let firstNumber: Double
     let secondNumber: Double
-    let operation: Operation
     
-    init(operation: Operation, firstNumber: Double, secondNumber: Double) {
-        self.operation = operation
+    init(firstNumber: Double, secondNumber: Double) {
         self.firstNumber = firstNumber
         self.secondNumber = secondNumber
     }
@@ -40,25 +34,7 @@ class Calculator {
     func result() -> Double {
         var result: Double = 0.0
         
-        switch operation {
-        case .add : result = AddOperation().result(firstNumber: firstNumber, secondNumber: secondNumber)
-        case .substract : result = SubstractOperation().result(firstNumber: firstNumber, secondNumber: secondNumber)
-        case .multiply : result = MultiplyOperation().result(firstNumber: firstNumber, secondNumber: secondNumber)
-        case .divide : result = DivideOperation().result(firstNumber: firstNumber, secondNumber: secondNumber)
-        }
-        
         return result
     }
  }
 
-let addResult = Calculator(operation: .add, firstNumber: 7, secondNumber: 3).result()
-print("덧셈 결과 \(addResult)")
-
-let substractResult = Calculator(operation: .substract, firstNumber: 7, secondNumber: 3).result()
-print("뺄셈 결과 \(substractResult)")
-
-let multiplyResult = Calculator(operation: .multiply, firstNumber: 7, secondNumber: 3).result()
-print("곱셈 결과 \(multiplyResult)")
-
-let divideResult = Calculator(operation: .divide, firstNumber: 7, secondNumber: 3).result()
-print("나눗셈 결과 \(divideResult)")
