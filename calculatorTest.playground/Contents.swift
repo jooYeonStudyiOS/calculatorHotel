73e86b237347338c8699db02b67b348728572dd0
jooYeon <conn0204@naver.com>
2023-07-19 12:35:14 +0900

    calculator 안에 있던 함수들을 class로 수정

--- calculatorTest.playground/Contents.swift
+++ calculatorTest.playground/Contents.swift
@@ -5,6 +5,30 @@ enum Operation {
     case divide
 }
 
+class AddOperation {
+    func result(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber + secondNumber
+    }
+}
+
+class SubstractOperation {
+    func result(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber - secondNumber
+    }
+}
+
+class MultiplyOperation {
+    func result(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber * secondNumber
+    }
+}
+
+class DivideOperation {
+    func result(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber / secondNumber
+    }
+}
+
 class Calculator {
     let firstNumber: Double
     let secondNumber: Double
@@ -13,43 +37,23 @@ class Calculator {
         self.firstNumber = firstNumber
         self.secondNumber = secondNumber
     }
-    
-    func plusOperation() -> Double {
-        return firstNumber + secondNumber
-    }
-    
-    func minusOperation() -> Double {
-        return firstNumber - secondNumber
-    }
-    
-    func multiplyOperation() -> Double {
-        return firstNumber * secondNumber
-    }
-    
-    func dividedOperation() -> Double {
-        return firstNumber / secondNumber
-    }
-    
-    func truncatingRemainderOperation() -> Double {
-        return firstNumber.truncatingRemainder(dividingBy: secondNumber)
-    }
 }
 
 let calculator = Calculator(firstNumber: 3, secondNumber: 7)
 let firstNumber = calculator.firstNumber
 let secondNumber = calculator.secondNumber
 
-let plusResult = calculator.plusOperation()
-print("\(firstNumber) + \(secondNumber) = \(plusResult)")
-
-let minusResult = calculator.minusOperation()
-print("\(firstNumber) - \(secondNumber) = \(minusResult)")
-
-let multiplyResult = calculator.multiplyOperation()
-print("\(firstNumber) * \(secondNumber) = \(multiplyResult)")
-
-let divideResult = calculator.dividedOperation()
-print("\(firstNumber) / \(secondNumber) = \(divideResult)")
-
-let remainderResult = calculator.truncatingRemainderOperation()
-print("\(firstNumber) % \(secondNumber) = \(remainderResult)")
+//let plusResult = calculator.plusOperation()
+//print("\(firstNumber) + \(secondNumber) = \(plusResult)")
+//
+//let minusResult = calculator.minusOperation()
+//print("\(firstNumber) - \(secondNumber) = \(minusResult)")
+//
+//let multiplyResult = calculator.multiplyOperation()
+//print("\(firstNumber) * \(secondNumber) = \(multiplyResult)")
+//
+//let divideResult = calculator.dividedOperation()
+//print("\(firstNumber) / \(secondNumber) = \(divideResult)")
+//
+//let remainderResult = calculator.truncatingRemainderOperation()
+//print("\(firstNumber) % \(secondNumber) = \(remainderResult)")
